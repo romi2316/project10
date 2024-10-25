7e8885e8840c072ae27519749ccfb948910af082
roumaissa-visions <roumaissa@visionspol.eu>
2024-10-25 15:38:53 +0200

    Added multiple test scenarios for login: error handling and field validation

--- cypress/e2e/connexion.cy.js
+++ cypress/e2e/connexion.cy.js
@@ -1,14 +1,35 @@
 
 describe("Connexion scénario", () => {
-    it('should successfully login and display the cart button', () => {
+    it('should successfully login with correct credentials and display the cart button', () => {
         cy.visit('http://localhost:8080/'); // home page
         cy.get('[data-cy="nav-link-login"]').click() //connexion navbar btn
         cy.url('http://localhost:8080/#/login').should('exist')
         cy.get('[data-cy="login-input-username"]').type('test2@test.fr'); // email 
         cy.get('[data-cy="login-input-password"]').type('testtest'); // password 
         cy.get('[data-cy="login-submit"]').click() // se connecter btn
-        .then(() => {
-        cy.get('[data-cy="nav-link-cart"]').should('exist')
-})
-})
+            .then(() => {
+                cy.get('[data-cy="nav-link-cart"]').should('exist')
+            })
+    })
+    it('should not login with incorrect credentials and display an error message', () => {
+        cy.visit('http://localhost:8080/'); // home page
+        cy.get('[data-cy="nav-link-login"]').click() //connexion navbar btn
+        cy.url('http://localhost:8080/#/login').should('exist')
+        cy.get('[data-cy="login-input-username"]').type('wrong@email.fr'); // email 
+        cy.get('[data-cy="login-input-password"]').type('wrongpassword'); // password 
+        cy.get('[data-cy="login-submit"]').click() // se connecter btn
+            .then(() => {
+                cy.get('[data-cy="login-errors"]').should('contain', 'Identifiants incorrects')
+            })
+    })
+
+    it('should display an error message when trying to login with empty fields', () => {
+        cy.visit('http://localhost:8080/'); // home page
+        cy.get('[data-cy="nav-link-login"]').click() //connexion navbar btn
+        cy.url('http://localhost:8080/#/login').should('exist')
+        cy.get('[data-cy="login-submit"]').click() // se connecter btn
+            .then(() => {
+                cy.get('[data-cy="login-errors"]').should('contain', 'Merci de remplir correctement tous les champs')
+            })
+    })
 })
